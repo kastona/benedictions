b29cb52813ed3e47a6b8fe4a97e41f0e8f6b6dde
Stephen Kastona <stephenkastona@gmail.com>
2020-02-20 08:04:12 +0100

    song model added

---
+++ src/models/song.js
@@ -0,0 +1,40 @@
+const mongoose = require('../db/mongoose')
+
+const songSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    length: {
+        type: Number
+    },
+    rating: {
+        type: Number,
+        validate(rating) {
+            if(rating < 1 || rating > 5) {
+                throw new Error('Invalid rating')
+            }
+        }
+    },
+    hits: {
+        type: Number
+    },
+    genre: {
+        type: String,
+        trim: true
+    },
+    songBuffer: {
+        type: Buffer
+    },
+    artist: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+},{timestamps: true})
+
+
+const Song = mongoose.model('Song', songSchema)
+
+module.exports = Song
